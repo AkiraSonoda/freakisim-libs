73783df82e6a8c0ffc24504342db62653c4bf688
ft- <ft@noemail>
2014-07-10 19:11:15 +0200

    added a prebuild compilable version of that HttpServer (some parts already simplified) still open: lock revisioning

---
+++ CSharp/HttpServer/Parser/RequestParserFactory.cs
@@ -0,0 +1,33 @@
+﻿using HttpServer.Parser;
+
+namespace HttpServer
+{
+    /// <summary>
+    /// Creates request parsers when needed.
+    /// </summary>
+    public class RequestParserFactory : IRequestParserFactory
+    {
+        /// <summary>
+        /// Create a new request parser.
+        /// </summary>
+        /// <param name="logWriter">Used when logging should be enabled.</param>
+        /// <returns>A new request parser.</returns>
+        public HttpRequestParser CreateParser(ILogWriter logWriter)
+        {
+            return new HttpRequestParser(logWriter);
+        }
+    }
+
+    /// <summary>
+    /// Creates request parsers when needed.
+    /// </summary>
+    public interface IRequestParserFactory
+    {
+        /// <summary>
+        /// Create a new request parser.
+        /// </summary>
+        /// <param name="logWriter">Used when logging should be enabled.</param>
+        /// <returns>A new request parser.</returns>
+        HttpRequestParser CreateParser(ILogWriter logWriter);
+    }
+}
---
+++ CSharp/HttpServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("HttpServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("HttpServer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
